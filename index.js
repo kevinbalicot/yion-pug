03f8119a335ee06b08a5c5744989870b6116af13
Kevin Balicot <kevin.balicot@MacBook-Pro-de-administrateur-4.local>
2017-03-20 11:56:27 +0100

    feat(chore): Add type

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const pug = require('pug');
 
 module.exports = {
+    type: 'renderer',
     handle: (req, res, app) => {
         res.render = (filepath, options = {}, status = 200) => {
             res.status(status).send(pug.renderFile(filepath, options));
         };
     }
 };
-
