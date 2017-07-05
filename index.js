ea1cb69674ddf18c258be46bd566e05b0753076a
Kevin Balicot <kevinbalicot@gmail.com>
2017-07-05 12:42:12 +0200

    feat(core): Add next callback

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const pug = require('pug');
 
 module.exports = {
     type: 'renderer',
-    handle: (req, res, app) => {
+    handle: (req, res, app, next) => {
         res.render = (filepath, options = {}, status = 200) => {
             res.status(status).send(pug.renderFile(filepath, options));
         };
+
+        next();
     }
 };
