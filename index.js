7cd2bc76e56d87326ff36eef565420e11bbdc4ef
kevin.balicot <kevin.balicot@pmsipilot.com>
2019-09-02 14:43:20 +0200

    feat: Add headers to response | :warkles:

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const pug = require('pug');
 module.exports = {
     type: 'renderer',
     handle: (req, res, app, next) => {
-        res.render = (filepath, options = {}, status = 200) => {
+        res.render = (filepath, options = {}, status = 200, headers = {}) => {
+            for (let name in headers) {
+                res.set(name, headers[name]);
+            }
+
             res.status(status).send(pug.renderFile(filepath, options));
         };
 
